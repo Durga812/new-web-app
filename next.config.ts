3afb520f4a5beaa777b7e2ffd06edc2d15d955bc
durga812 <durga@immigreat.ai>
2025-10-06 16:46:27 +0530

    fix profile image load by updateing nextjs config file

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,11 @@ const nextConfig: NextConfig = {
         hostname: "ehyddwnabgcolqicgouo.supabase.co",
         pathname: "/storage/v1/object/public/**",
       },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+        pathname: "/**",
+      },
     ],
   },
 };
